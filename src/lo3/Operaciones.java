7b4d1c3c7d7159417b16cd08416cb910eae82cf4
Luchosaque <luchosaque@gmail.com>
2021-06-08 12:42:32 +0200

    Empiezo M4

--- src/lo3/Operaciones.java
+++ src/lo3/Operaciones.java
@@ -11,6 +11,19 @@ package lo3;
  */
 public class Operaciones {
 
+    /**
+     * 
+     * el metodo "sumarPar" recibe 2 parametros, "a" y "b", tipo int y retorna un int
+     * al ser ejecutado suma los dos parametros y comprueba si el resultado
+     * es un numero par, de ser asi retorna un int, cuyo valor
+     * es igual a la suma de los parametros recibidos, en caso contrario,
+     * si es un numero impar, retornara un int cuyo valor será la suma
+     * de los parametros recibidos +1.
+     * 
+     * @param a
+     * @param b
+     * @return 
+     */
     public int sumarPar(int a, int b) { /*el metodo recibe dos enteros a y b*/
         int resultado = a + b;/*el entero resultado es igual a la suma de los enteros a y b*/
         if(resultado%2==0){ /*si el entero resultado es dividido entre 2 y es igual a 0*/
@@ -20,6 +33,16 @@ public class Operaciones {
         }//end if
     }//end metodo
 
+    /**
+     * 
+     * el metodo "mayor" recibe 2 parametros, "a" y "b", de tipo int y retorna un entero
+     * al ser ejecutado comprueba que parametro es mayor, "a" o "b", y devuelve
+     * el mayor de ellos pero, de ser iguales, devuelve cualquiera de los dos parametros
+     * 
+     * @param a
+     * @param b
+     * @return 
+     */
     public int mayor(int a, int b) {/*el metodo recibe dos enteros a y b*/
         if(a>b){/*si el entero a es mayor q el entero b*/
             return a;/*devuelve el entero a*/
@@ -29,6 +52,15 @@ public class Operaciones {
     }//end metodo
     
     
+    /**
+     * 
+     * el metodo "sumarVector" recibe un parametro tipo array de int y devuelve un int
+     * al ejecutarse suma el valor de los numeros que componen el array y el resultado
+     * lo retorna en un int
+     * 
+     * @param numeros
+     * @return 
+     */
     public int sumarVector(int [] numeros) {/*el metodo recibe un array de numeros*/
         int sum=0;//el entero sum es igual a cero
         for (int i = 0; i < numeros.length; i++) {//para i igual a cero; i menor que la longitud del array numeros;i aumenta uno
